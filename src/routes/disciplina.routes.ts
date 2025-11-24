0a2112e798cabb2eae3b818b05bc63ce79d36807
Halbert Nascimento <halbert.fac@gmail.com>
2025-11-23 23:51:01 -0300

    Adiciona middleware de autenticação e permissão nas rotas de disciplina

--- src/routes/disciplina.routes.ts
+++ src/routes/disciplina.routes.ts
@@ -7,10 +7,15 @@ import {
 	createCursoDisciplina,
 } from "../controller/disciplinaController";
 
+// importa os guardas
+import { authMiddleware } from "../middleware/authMiddleware";
+import { preventProfessorEdit } from "../middleware/permissionMiddleware";
+
+
 const router = express.Router();
 
 //Rotas Perfil
-router.get("/", getDisciplina); // GET /disciplina
+router.get("/",  getDisciplina); // GET /disciplina
 router.get("/:idDisciplina", getDisciplinaById); // GET /disciplina/idDisciplina
 router.get("/curso/:idCurso", getDisciplinaByCurso); // GET /disciplina/curso/idCurso
 router.post("/", createDisciplina); // POST /disciplina
