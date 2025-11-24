fc45d821054da55a13bc6b5c8db8d43e41f0ebb2
Halbert Nascimento <halbert.fac@gmail.com>
2025-11-23 22:18:10 -0300

    Adiciona rota de autenticação com endpoint de login

--- src/routes/app.ts
+++ src/routes/app.ts
@@ -13,6 +13,8 @@ import professorDisciplinaRoutes from "./professorDisciplina.routes";
 import usuarioRoutes from "./usuario.routes";
 import salaRoutes from "./sala.routes";
 
+import authRoutes from "./auth.routes";
+
 const app = express();
 
 app.use(
@@ -31,16 +33,18 @@ app.get("/", (req: Request, res: Response) => {
 	res.status(200).json({ message: "API Grade Horário está funcionando!" });
 });
 
-app.use("/celula", celulaRoutes);
-app.use("/curso", cursoRoutes);
-app.use("/disciplina", disciplinaRoutes);
-app.use("/professor", professorRoutes);
-app.use("/grade", gradeRoutes);
-app.use("/diaSemana", diaSemanaRoutes);
-app.use("/disponibilidade", disponibilidadeRoutes);
-app.use("/professorDisciplina", professorDisciplinaRoutes);
-app.use("/usuario", usuarioRoutes);
-app.use("/sala", salaRoutes);
+app.use("/auth", authRoutes); // Rota para autenticação, ficam: /auth/login
+
+app.use("/celula", celulaRoutes); // Rota para células, ficam: /celula/...
+app.use("/curso", cursoRoutes); // Rota para cursos, ficam: /curso/...
+app.use("/disciplina", disciplinaRoutes); 	// Rota para disciplinas, ficam: /disciplina/...
+app.use("/professor", professorRoutes); // Rota para professores, ficam: /professor/...
+app.use("/grade", gradeRoutes); // Rota para grades, ficam: /grade/...
+app.use("/diaSemana", diaSemanaRoutes); // Rota para dias da semana, ficam: /diaSemana/...
+app.use("/disponibilidade", disponibilidadeRoutes); // Rota para disponibilidades, ficam: /disponibilidade/...
+app.use("/professorDisciplina", professorDisciplinaRoutes); // Rota para professor-disciplinas, ficam: /professorDisciplina/...
+app.use("/usuario", usuarioRoutes); // Rota para usuários, ficam: /usuario/...
+app.use("/sala", salaRoutes); // Rota para salas, ficam: /sala/...
 
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
 	console.error(err.stack);
